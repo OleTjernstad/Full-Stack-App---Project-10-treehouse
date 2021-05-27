1751bb5a9d058adc0ed7f68f7a6553b39633cd75
Ole Tjernstad <ott556@gmail.com>
2021-05-27 06:52:31 +0200

    add axios with baseURL

--- client/src/api/server.js
+++ client/src/api/server.js
@@ -1,15 +1,5 @@
-import apiKey from '../config';
+import axios from 'axios';
 
-const photoQueryUrl = (tag, prPage) => {
-    return `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tag}&per_page=${prPage}&format=json&nojsoncallback=1`;
-};
-
-export const queryPhotos = async (query) => {
-    const response = await fetch(photoQueryUrl(query, 24));
-    const json = await response.json();
-    return json.photos.photo;
-};
-
-export const photoUrl = (serverId, id, secret) => {
-    return `https://live.staticflickr.com/${serverId}/${id}_${secret}.jpg`;
-};
+export default axios.create({
+    baseURL: process.env.REACT_APP_BASE_API_URL
+});
