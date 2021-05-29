2ab5783d4aad8565b24acc883d76d695ef4f722b
Ole Tjernstad <ott556@gmail.com>
2021-05-29 10:16:45 +0200

    signup new user

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Courses } from './views/courses';
 import { Header } from './layout/header';
 import { PrivateRoute } from './routes/privateRoute';
 import { UserSignOut } from './views/user-sign-out';
+import { UserSignUp } from './views/user-sign-up';
 import { UserSignin } from './views/user-signIn';
 
 function App() {
@@ -16,6 +17,9 @@ function App() {
                     <Switch>
                         <Route exact path="/">
                             <Courses />
+                        </Route>
+                        <Route path="/signup">
+                            <UserSignUp />
                         </Route>
                         <Route path="/signin">
                             <UserSignin />
---
+++ client/src/views/user-sign-up.js
@@ -0,0 +1,83 @@
+import { Link, useHistory } from 'react-router-dom';
+
+import { server } from '../api/server';
+import { useRef } from 'react';
+
+export const UserSignUp = () => {
+    const history = useHistory();
+
+    const firstName = useRef();
+    const lastName = useRef();
+    const emailAddress = useRef();
+    const password = useRef();
+    const confirmPassword = useRef();
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+
+        if (password.current.value === confirmPassword.current.value) {
+            const { status } = await server.post('api/users', {
+                firstName: firstName.current.value,
+                lastName: lastName.current.value,
+                emailAddress: emailAddress.current.value,
+                password: password.current.value
+            });
+            if (status === 201) {
+                history.replace('/signin');
+            }
+        }
+    };
+    return (
+        <main>
+            <div className="form--centered">
+                <h2>Sign Up</h2>
+
+                <form onSubmit={handleSignUp}>
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                        ref={firstName}
+                        id="firstName"
+                        name="firstName"
+                        type="text"
+                    />
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                        ref={lastName}
+                        id="lastName"
+                        name="lastName"
+                        type="text"
+                    />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input
+                        ref={emailAddress}
+                        id="emailAddress"
+                        name="emailAddress"
+                        type="email"
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        ref={password}
+                        id="password"
+                        name="password"
+                        type="password"
+                    />
+                    <label htmlFor="confirmPassword">Confirm Password</label>
+                    <input
+                        ref={confirmPassword}
+                        id="confirmPassword"
+                        name="confirmPassword"
+                        type="password"
+                    />
+                    <button className="button" type="submit">
+                        Sign Up
+                    </button>
+                    <button className="button button-secondary">Cancel</button>
+                </form>
+                <p>
+                    Already have a user account? Click here to{' '}
+                    <Link to="/signin">Sign In</Link>!
+                </p>
+            </div>
+        </main>
+    );
+};
