ac0df929d0c21c678d94a8b3f1b1538f139e5cfc
Ole Tjernstad <ott556@gmail.com>
2021-05-30 11:05:04 +0200

    create course

---
+++ client/src/components/course-form.js
@@ -0,0 +1,94 @@
+export const CourseForm = ({
+    course: { title, User, description, estimatedTime, materialsNeeded },
+    dispatch
+}) => {
+    return (
+        <div className="main--flex">
+            <div>
+                <label htmlFor="courseTitle">Course Title</label>
+                <input
+                    id="courseTitle"
+                    name="courseTitle"
+                    type="text"
+                    value={title}
+                    onChange={(e) =>
+                        dispatch({ type: 'setTitle', payload: e.target.value })
+                    }
+                />
+
+                <p>
+                    By {User.firstName} {User.lastName}
+                </p>
+
+                <label htmlFor="courseDescription">Course Description</label>
+                <textarea
+                    id="courseDescription"
+                    name="courseDescription"
+                    value={description}
+                    onChange={(e) =>
+                        dispatch({
+                            type: 'setDescription',
+                            payload: e.target.value
+                        })
+                    }
+                ></textarea>
+            </div>
+            <div>
+                <label htmlFor="estimatedTime">Estimated Time</label>
+                <input
+                    id="estimatedTime"
+                    name="estimatedTime"
+                    type="text"
+                    value={estimatedTime}
+                    onChange={(e) =>
+                        dispatch({
+                            type: 'setEstimatedTime',
+                            payload: e.target.value
+                        })
+                    }
+                />
+
+                <label htmlFor="materialsNeeded">Materials Needed</label>
+                <textarea
+                    id="materialsNeeded"
+                    name="materialsNeeded"
+                    value={materialsNeeded}
+                    onChange={(e) =>
+                        dispatch({
+                            type: 'SetMaterialsNeeded',
+                            payload: e.target.value
+                        })
+                    }
+                ></textarea>
+            </div>
+        </div>
+    );
+};
+
+export const initialState = {
+    id: 0,
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialsNeeded: '',
+    User: {}
+};
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'setId':
+            return { ...state, id: action.payload };
+        case 'setTitle':
+            return { ...state, title: action.payload };
+        case 'setDescription':
+            return { ...state, description: action.payload };
+        case 'setEstimatedTime':
+            return { ...state, estimatedTime: action.payload };
+        case 'SetMaterialsNeeded':
+            return { ...state, materialsNeeded: action.payload };
+        case 'setUser':
+            return { ...state, User: action.payload };
+        default:
+            throw new Error();
+    }
+};
--- client/src/views/create-course.js
+++ client/src/views/create-course.js
@@ -1,63 +1,42 @@
+import { CourseForm, initialState, reducer } from '../components/course-form';
+
+import { server } from '../api/server';
+import { useAuth } from '../hooks/useAuth';
+import { useHistory } from 'react-router-dom';
+import { useReducer } from 'react';
+
 export const CreateCourse = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const history = useHistory();
+    const { user } = useAuth();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const { status } = await server.post('api/courses', {
+            ...state,
+            userId: user.id
+        });
+        if (status === 201) {
+            history.replace('/');
+        }
+    };
     return (
         <main>
-            <div class="wrap">
+            <div className="wrap">
                 <h2>Create Course</h2>
-                <div class="validation--errors">
+                <div className="validation--errors">
                     <h3>Validation Errors</h3>
                     <ul>
                         <li>Please provide a value for "Title"</li>
                         <li>Please provide a value for "Description"</li>
                     </ul>
                 </div>
-                <form>
-                    <div class="main--flex">
-                        <div>
-                            <label for="courseTitle">Course Title</label>
-                            <input
-                                id="courseTitle"
-                                name="courseTitle"
-                                type="text"
-                                value=""
-                            />
-
-                            <p>By Joe Smith</p>
-
-                            <label for="courseDescription">
-                                Course Description
-                            </label>
-                            <textarea
-                                id="courseDescription"
-                                name="courseDescription"
-                            ></textarea>
-                        </div>
-                        <div>
-                            <label for="estimatedTime">Estimated Time</label>
-                            <input
-                                id="estimatedTime"
-                                name="estimatedTime"
-                                type="text"
-                                value=""
-                            />
-
-                            <label for="materialsNeeded">
-                                Materials Needed
-                            </label>
-                            <textarea
-                                id="materialsNeeded"
-                                name="materialsNeeded"
-                            ></textarea>
-                        </div>
-                    </div>
-                    <button class="button" type="submit">
+                <form onSubmit={handleSubmit}>
+                    <CourseForm course={state} dispatch={dispatch} />
+                    <button className="button" type="submit">
                         Create Course
                     </button>
-                    <button
-                        class="button button-secondary"
-                        onclick="event.preventDefault(); location.href='index.html';"
-                    >
-                        Cancel
-                    </button>
+                    <button className="button button-secondary">Cancel</button>
                 </form>
             </div>
         </main>
